6ea974df2a3c3a4a05165d65ba65a2dad4b95251
matfij <mateusz.michal.fijak@gmail.com>
2023-03-18 20:04:23 +0100

    setup ingress service

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -3,6 +3,10 @@ import express, { json } from "express";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentUser", (req, res) => {
+  res.send("hello");
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
 });
