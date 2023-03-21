671279abc1491d6e81a19bc93b531239bafac409
matfij <mateusz.michal.fijak@gmail.com>
2023-03-21 21:12:00 +0100

    implement user sign in

--- apps/auth/src/routes/auth-router.ts
+++ apps/auth/src/routes/auth-router.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { body } from "express-validator";
-import { validationResult } from "express-validator/src/validation-result";
 import { sign } from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
 import { BadRequestApiError } from "../models/bad-request-api-error";
-import { DatabaseApiError } from "../models/database-api-error";
 import { User } from "../models/user";
-import { ValidationApiError } from "../models/validation-api-error";
+import { PasswordService } from "../services/password-service";
 
 const authRouter = Router();
 
@@ -18,11 +17,8 @@ authRouter.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Invalid password"),
   ],
+  validateRequest,
   async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return next(new ValidationApiError(errors.array()));
-    }
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -45,9 +41,37 @@ authRouter.post(
   }
 );
 
-authRouter.post("/api/users/signIn", (req, res) => {
-  res.send("<h2>Hello there, sign in</h2>");
-});
+authRouter.post(
+  "/api/users/signIn",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password").trim().notEmpty().withMessage("Invalid password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return next(new BadRequestApiError("Invalid credentials"));
+    }
+    const passwordMatch = await PasswordService.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      return next(new BadRequestApiError("Invalid credentials"));
+    }
+    const userJwt = sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    req.session = { jwt: userJwt };
+    res.status(200).send(existingUser);
+  }
+);
 
 authRouter.post("/api/users/signOut", (req, res) => {
   res.send("<h2>Hello there, sign out</h2>");
