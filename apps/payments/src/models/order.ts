c64e687c117e160ad7dc960c2107dfa248b15708
matfij <mateusz.michal.fijak@gmail.com>
2023-04-12 20:20:24 +0200

    implement order model

---
+++ apps/payments/src/models/order.ts
@@ -0,0 +1,65 @@
+import { OrderStatus } from "@nightwood/common";
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface OrderAttrs {
+  id: string;
+  price: number;
+  status: OrderStatus;
+  userId: string;
+  version: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  price: number;
+  status: OrderStatus;
+  userId: string;
+  version: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    price: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+orderSchema.set("versionKey", "version");
+
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    price: attrs.price,
+    status: attrs.status,
+    userId: attrs.userId,
+    version: attrs.version,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
